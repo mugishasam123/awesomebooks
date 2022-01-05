24304b8a64b1f67b9ec1af678066b5ad35238d5a
matovu-farid <matovu90@gmail.com>
2022-01-05 21:00:57 +0300

    Add navigation and styling

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import BookManager from './book_manager.js';
 import DisplayBooks from './display_books.js';
 import FormListener from './form_listener.js';
+import Navigation from './navigation.js'
 
 const bookManager = new BookManager();
 const displayBooks = new DisplayBooks(bookManager.bookCollection);
 displayBooks.display();
 const formListener = new FormListener(bookManager);
 formListener.addEventListener();
+const navigation = new Navigation();
+navigation.attachListeners();
